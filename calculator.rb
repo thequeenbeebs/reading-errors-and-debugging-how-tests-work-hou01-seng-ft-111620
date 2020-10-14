0c261a98c02e4d35ce785d24d22794b3b4c4d76e
Blaire Baker <bakerblaire@gmail.com>
2020-10-14 23:31:29 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 6
+second_number = 13
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
